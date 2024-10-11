e6aad04b84477ede7eb7c4ff5de630a936411d8c
KhoslaGA <90459399+KhoslaGA@users.noreply.github.com>
2024-10-11 14:08:28 -0400

    Nested Routes

---
+++ my-app/src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const blog = (props: Props) => {
+  return <div>Blog page</div>;
+};
+
+export default blog;
---
+++ my-app/src/app/blog/second/page.tsx
@@ -0,0 +1,3 @@
+export default function secondblog() {
+  return <div>Seon blog!</div>;
+}
