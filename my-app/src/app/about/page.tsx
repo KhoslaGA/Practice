f20d5b4b0a463bd268cc23bcae26230fe82518f5
KhoslaGA <90459399+KhoslaGA@users.noreply.github.com>
2024-10-14 11:27:28 -0400

    Page metadata >> layout metadata

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -1,1 +1,7 @@
-export default function About() {}
+export const metadata = {
+  title: "About Next js",
+};
+
+export default function About() {
+  return <h1>About Next js</h1>;
+}
--- my-app/src/app/products/[productId]/page.tsx
+++ my-app/src/app/products/[productId]/page.tsx
@@ -1,7 +1,29 @@
-export default function ProductDetail({
+import { Metadata } from "next";
+
+interface Props {
+  params: {
+    productId: string;
+  };
+}
+
+export const generateMetaData = async ({
   params,
-}: {
-  params: { productId: string };
-}) {
-  return <div>Product details Dynamic and id is {params.productId}</div>;
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Iphone ${params.productId}`);
+    });
+  });
+  return {
+    title: `Buy ${title} Online - Best Price, Deals & Discounts`,
+  };
+};
+
+export default function ProductPage({ params }: Props) {
+  return (
+    <>
+      <h1>Product Details</h1>
+      <p>Product ID: {params.productId}</p>
+    </>
+  );
 }
