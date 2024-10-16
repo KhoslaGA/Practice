221b09fe18f6f843cdc904080af0cdf98272bc7d
KhoslaGA <90459399+KhoslaGA@users.noreply.github.com>
2024-10-15 22:47:24 -0400

    Using templates in Next js for reseting input

--- my-app/src/app/(auth)/(with-auth-layout)/template.tsx
+++ my-app/src/app/(auth)/(with-auth-layout)/template.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 // * used to create Active Links
 import { usePathname } from "next/navigation";
 import "../styles.css";
+import { useState } from "react";
 
 const navlinks = [
   { name: "login", href: "/login" },
@@ -16,9 +17,18 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   const pathName = usePathname();
+  // * we have to do e.preventdefault() but we can rename layout.tsx as template.tsx and the job is done by Next js automatically
+  const [input, setInput] = useState("");
 
   return (
     <div>
+      <div>
+        <input
+          className="border-red-700"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
       {navlinks.map((link) => {
         const isActive = pathName.startsWith(link.href);
         return (
