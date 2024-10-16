69cb7067a902dd5695c5c1632797df06ad28381b
KhoslaGA <90459399+KhoslaGA@users.noreply.github.com>
2024-10-15 22:51:21 -0400

    Loading file in a specific component

---
+++ my-app/src/app/blog/loading.tsx
@@ -0,0 +1,6 @@
+export default function Loading() {
+  return <h1>Loading</h1>;
+}
+
+// * you can display loading state immediately, application becomes more responsive
+// * Very useful in components with shared layouts
